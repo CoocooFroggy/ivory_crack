e319424662e25a1b48e08f0a8b6918fe2e415e79
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2023-04-17 11:31:37 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ivory_crack
+
+Working towards solving the Ivory hidden messages in "I Killed God."
+
+Tries every combination of `0` and `O` in base64 because it's hard to tell which is which in the video.
---
+++ bin/ivory_crack.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+void main(List<String> arguments) {
+  final original = 'rmJOO8k4ONy13+MmIA==';
+  printAllCombinations(original);
+}
+
+void printAllCombinations(String inputString) {
+  // Find the indices of all "O" characters in the input string
+  List<int> oIndices = [];
+  for (int i = 0; i < inputString.length; i++) {
+    if (inputString[i] == 'O') {
+      oIndices.add(i);
+    }
+  }
+
+  // Generate all possible combinations of 0s and 1s for the "O" characters
+  for (int i = 0; i < (1 << oIndices.length); i++) {
+    // Create a new string to hold the output
+    String outputString = '';
+    // Keep track of which "O" character we are currently processing
+    int oIndex = 0;
+    // Loop through each character in the input string
+    for (int j = 0; j < inputString.length; j++) {
+      if (oIndex < oIndices.length && j == oIndices[oIndex]) {
+        // If this is an "O" character, replace it with a 0 or * depending on the current bit value in the bit mask
+        outputString += ((i & (1 << oIndex)) > 0) ? '*' : '0';
+        // Move to the next "O" character
+        oIndex++;
+      } else {
+        // If this is not an "O" character, keep the original character
+        outputString += inputString[j];
+      }
+    }
+
+    // Print the generated string to the console
+    outputString = outputString.replaceAll('*', 'O');
+    print('---');
+    print(outputString);
+    print(utf8.decode(base64Decode(outputString), allowMalformed: true));
+  }
+}
