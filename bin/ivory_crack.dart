e51e28c485432b8c000a0b7f46ca187da8cc2fb0
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2023-04-17 23:09:47 -0400

    New method, cleaner

--- bin/ivory_crack.dart
+++ bin/ivory_crack.dart
@@ -1,44 +1,81 @@
 import 'dart:convert';
 
 void main(List<String> arguments) {
-  final original = 'rmJ008k4ONy13+MmIA==';
-  // Replace all zeros with O because that's how the program works
-  final workable = original.replaceAll('0', 'O');
-  printAllCombinations(workable);
+  final pieces = [
+    ['V7'],
+    ['rm', 'nm'],
+    ['JO', 'Jo', 'J0'],
+    ['O8', '08'],
+    ['k4'],
+    ['ON', '0N', 'oN'],
+    ['y1'],
+    ['eI', 'el'],
+    ['3+'],
+    ['Mm'],
+    ['IA', 'lA'],
+    ['=='],
+  ];
+
+  final choiceIndexes = [
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+  ];
+
+  // final pieces = [
+  //   ['01', '02', '03'],
+  //   ['AA', 'BB'],
+  //   ['IA', 'lA'],
+  //   ['=='],
+  // ];
+  //
+  // final choiceIndexes = [
+  //   0,
+  //   0,
+  //   0,
+  //   0,
+  // ];
+
+  iterateFromIndex(pieces, 0, choiceIndexes);
 }
 
-void printAllCombinations(String inputString) {
-  // Find the indices of all "O" characters in the input string
-  List<int> oIndices = [];
-  for (int i = 0; i < inputString.length; i++) {
-    if (inputString[i] == 'O') {
-      oIndices.add(i);
+void iterateFromIndex(
+    List<List<String>> pieces, int index, List<int> choiceIndexes) {
+  for (var choice in pieces[index]) {
+    // If we are not on the last index
+    if (index != choiceIndexes.length - 1) {
+      // Do all iterations under our piece
+      iterateFromIndex(pieces, index + 1, choiceIndexes);
+    } else {
+      // Only print if we're at the end of the chain
+      final currentB64 = compile(pieces, choiceIndexes);
+      print('---');
+      print(currentB64);
+      print(utf8.decode(base64Decode(currentB64), allowMalformed: true));
+    }
+    // If we have other choices, try them
+    if (choiceIndexes[index] + 1 < pieces[index].length) {
+      choiceIndexes[index] += 1;
     }
   }
+  choiceIndexes[index] = 0;
+}
 
-  // Generate all possible combinations of 0s and 1s for the "O" characters
-  for (int i = 0; i < (1 << oIndices.length); i++) {
-    // Create a new string to hold the output
-    String outputString = '';
-    // Keep track of which "O" character we are currently processing
-    int oIndex = 0;
-    // Loop through each character in the input string
-    for (int j = 0; j < inputString.length; j++) {
-      if (oIndex < oIndices.length && j == oIndices[oIndex]) {
-        // If this is an "O" character, replace it with a 0 or * depending on the current bit value in the bit mask
-        outputString += ((i & (1 << oIndex)) > 0) ? '*' : '0';
-        // Move to the next "O" character
-        oIndex++;
-      } else {
-        // If this is not an "O" character, keep the original character
-        outputString += inputString[j];
-      }
-    }
-
-    // Print the generated string to the console
-    outputString = outputString.replaceAll('*', 'O');
-    print('---');
-    print(outputString);
-    print(utf8.decode(base64Decode(outputString), allowMalformed: true));
+String compile(List<List<String>> pieces, List<int> choiceIndexes) {
+  final buffer = StringBuffer();
+  // Loop through all pieces
+  for (int i = 0; i < pieces.length; i++) {
+    // Choose that index
+    buffer.write(pieces[i][choiceIndexes[i]]);
   }
+  return buffer.toString();
 }
